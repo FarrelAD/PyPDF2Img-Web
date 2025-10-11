37af0c9c43b27278e489bccc18314956fc8e5240
Anuraj-IND <anuraj.m.11.04@gmail.com>
2025-10-11 15:21:15 +0530

    updated UI and readme file

---
+++ pdf2img_web/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for pdf2img_web project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdf2img_web.settings')
+
+application = get_wsgi_application()
